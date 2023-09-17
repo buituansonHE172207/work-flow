1c1b3ffaad0663bd25b6b5dc0d61df55b404c1e6
Bui Tuan Son <SonBTHE172207@fpt.edu.vn>
2023-09-17 20:22:30 +0700

    #6 - add table image

--- src/main/java/com/test/test_git/model/Image.java
+++ src/main/java/com/test/test_git/model/Image.java
@@ -1,5 +1,6 @@
 package com.test.test_git.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,37 +11,25 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 @Builder
-@Table(name = "order_detail")
 @Entity
-public class OrderDetail {
-
+@Table (name = "image")
+class Image {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private Long id;
 
-    
     @ManyToOne
-    @JoinColumn(name = "order_id")
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Order order;
-
-    @ManyToOne
-    @JoinColumn(name = "book_id")
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
+    @JoinColumn (name = "book_id")
     private Book book;
 
-    private int amount;
+    private String link;
 
-    private int unitPrice;
+    private String description;
 }
-
